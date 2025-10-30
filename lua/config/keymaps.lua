33e33e7437a29010ff2eede9134c4a638758e061
erewl <grunert.katrin@protonmail.com>
2025-10-30 14:37:16 +0100

    Comment out invalid keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,8 +10,9 @@ if ok then
 	local map = vim.keymap.set
 	local opts = { silent = true }
   -- Run and debug commands
-  map("n", "<leader>gr", godot.run, opts)       -- Run the project
-  map("n", "<leader>gl", godot.run_last, opts)  -- Run the last scene/project
-  map("n", "<leader>gd", godot.debug, opts)     -- Start debugger
-  map("n", "<leader>gs", godot.stop, opts)      -- Stop the running instance
+  -- Not valid functions at the moment
+  -- map("n", "<leader>gr", godot.run, opts)       -- Run the project
+  -- map("n", "<leader>gl", godot.run_last, opts)  -- Run the last scene/project
+  -- map("n", "<leader>gd", godot.debug, opts)     -- Start debugger
+  -- map("n", "<leader>gs", godot.stop, opts)      -- Stop the running instance
 end
